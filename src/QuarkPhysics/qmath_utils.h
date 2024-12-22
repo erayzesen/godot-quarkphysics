0239f7063d87de7016591ba0e2bc2c12f4b353c2
Eray Zesen <erayzesen@gmail.com>
2024-12-22 03:50:39 +0300

    github actions settings 10

--- src/QuarkPhysics/qmath_utils.h
+++ src/QuarkPhysics/qmath_utils.h
@@ -1,8 +1,14 @@
 #ifndef MATH_UTILS_H
 #define MATH_UTILS_H
 
+#include <cfloat>
+
 #ifndef M_PI
     #define M_PI 3.14159265358979323846
+#endif
+
+#ifndef MAXFLOAT
+    #define MAXFLOAT FLTMAX
 #endif
 
 #endif // MATH_UTILS_H
